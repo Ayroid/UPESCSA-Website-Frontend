ef9401faacd5f5d50dd08f5aaa20a197e7a0f051
Ayroid <ayushsk0000@gmail.com>
2024-01-19 14:39:35 +0530

    css variables added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,19 @@
-import NavLink from "../../components/NavLink/NavLink";
+import NavLink from "../../components/NavItems/NavItems";
 
 const HomePage = () => {
   const navLinks = [
-    { title: "Home", path: "/" },
+    { title: "About", action: "#about" },
+    { title: "Events", action: "#events" },
+    { title: "Team", action: "#team" },
+    { title: "CSR", action: "#csr" },
+    { title: "Blogs", action: "#blogs" },
+    { title: "Contact", action: "#contact" },
   ];
 
   return (
     <div>
-      {navLinks.map(({ title }) => (
-        <NavLink key={title} title={title} />
+      {navLinks.map(({ title, action }) => (
+        <NavLink key={title} action={action} title={title} />
       ))}
     </div>
   );
