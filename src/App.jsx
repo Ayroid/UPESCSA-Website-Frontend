40b3747d4715b9aa1ef3e8fde329e66d8dedb4f6
devanshtelanga <115074052+devanshtelanga@users.noreply.github.com>
2024-01-21 00:41:48 +0530

    404 Page done
    404 Page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
 import Csr from "./pages/Csr/Csr";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/csr" element={<Csr />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/csr" element={<Csr />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
   );
 }
 export default App;
