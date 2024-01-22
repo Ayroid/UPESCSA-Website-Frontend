c59cd50ca6d4022f360be839e62e97f8bc7877d5
Ayroid <ayushsk0000@gmail.com>
2024-01-22 13:40:04 +0530

    404 Page Modified

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,17 +1,13 @@
-import { Link } from "react-router-dom";
 import styles from "./PageNotFound.module.css";
 
 // CSS STYLES
-const { pageNotFound, subHeading, heading, homeButton } = styles;
+const { pageNotFound, subHeading, heading } = styles;
 
 const PageNotFound = () => {
   return (
     <div className={pageNotFound}>
       <div className={heading}>404</div>
       <div className={subHeading}>Page Not Found</div>
-      <Link to={"/"}>
-        <button className={homeButton}>Back to Home</button>
-      </Link>
     </div>
   );
 };
