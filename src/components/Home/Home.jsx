f322d1c3fa6fd6061fb01314ab6b556697f0ea71
Ayroid <ayushsk0000@gmail.com>
2024-01-22 23:13:58 +0530

    separate home component created

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,44 @@
+import styles from "./Home.module.css";
+
+import Socials from "../Socials/Socials";
+
+// CSS STYLES
+const {
+  homePage,
+  bgCircles,
+  outerCircle,
+  innerCircle,
+  socialsDiv,
+  arrowDiv,
+  pageContent,
+  verticalLine,
+  pageHeading,
+  verticalLine1,
+  verticalLine2,
+} = styles;
+
+const Home = () => {
+  return (
+    <div className={homePage}>
+      <div className={bgCircles}>
+        <div className={outerCircle}>
+          <div className={innerCircle}></div>
+        </div>
+      </div>
+      <div className={socialsDiv}>
+        <Socials orientation={"column"} />
+      </div>
+      <div className={arrowDiv}>
+        <img src="/icons/triangleArrow.png" alt="arrow" />
+      </div>
+      <div className={pageContent}>
+        <div className={pageHeading}>UPES</div>
+        <hr className={verticalLine} id={verticalLine1} />
+        <hr className={verticalLine} id={verticalLine2} />
+        <div className={pageHeading}>CSA</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,44 +1,7 @@
-import styles from "./HomePage.module.css";
-
-import Socials from "../../components/Socials/Socials";
-
-// CSS STYLES
-const {
-  homePage,
-  bgCircles,
-  outerCircle,
-  innerCircle,
-  socialsDiv,
-  arrowDiv,
-  pageContent,
-  verticalLine,
-  pageHeading,
-  verticalLine1,
-  verticalLine2,
-} = styles;
+import Home from "../../components/Home/Home";
 
 const HomePage = () => {
-  return (
-    <div className={homePage}>
-      <div className={bgCircles}>
-        <div className={outerCircle}>
-          <div className={innerCircle}></div>
-        </div>
-      </div>
-      <div className={socialsDiv}>
-        <Socials orientation={"column"} />
-      </div>
-      <div className={arrowDiv}>
-        <img src="/icons/triangleArrow.png" alt="arrow" />
-      </div>
-      <div className={pageContent}>
-        <div className={pageHeading}>UPES</div>
-        <hr className={verticalLine} id={verticalLine1} />
-        <hr className={verticalLine} id={verticalLine2} />
-        <div className={pageHeading}>CSA</div>
-      </div>
-    </div>
-  );
+  return <Home />;
 };
 
 export default HomePage;
