9a475f759c2a6986dc2673654e8b0d90f0ebefc6
Ayroid <ayushsk0000@gmail.com>
2024-01-23 23:45:36 +0530

    alliance section ready

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
-import About from "../../components/About/About";
 import Home from "../../components/Home/Home";
+import About from "../../components/About/About";
+import Alliance from "../../components/Alliance/Alliance";
 
 const HomePage = () => {
   return (
     <>
       <Home />
       <About />
+      <Alliance />
     </>
   );
 };
