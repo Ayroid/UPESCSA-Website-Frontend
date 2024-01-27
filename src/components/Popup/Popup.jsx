afa5021c91fad8462a8f7a169cad8c02e84a2966
Ayroid <ayushsk0000@gmail.com>
2024-01-27 16:19:31 +0530

    popupready

---
+++ src/components/Popup/Popup.jsx
@@ -0,0 +1,54 @@
+import Proptypes from "prop-types";
+import { Link } from "react-router-dom";
+import styles from "./Popup.module.css";
+
+// CSS STYLES
+const {
+  popupContainer,
+  popupDiv,
+  popupImage,
+  popupTextDiv,
+  popupHeading,
+  popupText,
+  popupRegisterButton,
+  popupCloseButton,
+} = styles;
+
+const Popup = ({
+  eventPosterURL,
+  eventHeading,
+  eventText,
+  eventFormURL,
+  closePopup,
+}) => {
+  return (
+    <div className={popupContainer}>
+      <div className={popupDiv}>
+        <div className={popupImage}>
+          <img src={eventPosterURL} alt="eventPoster" />
+        </div>
+        <div className={popupTextDiv}>
+          <h4 className={popupHeading}>{eventHeading}</h4>
+          <p className={popupText}>{eventText}</p>
+          <Link to={eventFormURL} className={popupRegisterButton}>
+            <p>REGISTER NOW</p>
+            <img src="/icons/rightArrow.png" alt="arrow" />
+          </Link>
+        </div>
+        <div className={popupCloseButton} onClick={closePopup}>
+          <img src="/icons/hamburgercross.png" alt="cross" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Popup.propTypes = {
+  eventPosterURL: Proptypes.string.isRequired,
+  eventHeading: Proptypes.string.isRequired,
+  eventText: Proptypes.string.isRequired,
+  eventFormURL: Proptypes.string.isRequired,
+  closePopup: Proptypes.func.isRequired,
+};
+
+export default Popup;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,23 @@
+import styles from "./HomePage.module.css";
+
 import Home from "../../components/Home/Home";
 import About from "../../components/About/About";
 import HomeEvents from "../../components/HomeEvents/HomeEvents";
 import Alliance from "../../components/Alliance/Alliance";
 import HomeBlogs from "../../components/HomeBlogs/HomeBlogs";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Popup from "../../components/Popup/Popup";
+
+// CSS STYLES
+const { homepageContainer } = styles;
 
 const HomePage = () => {
-  const currentHash = window.location.hash;
+  // STATES
+  const [popupOpen, setPopupOpen] = useState(false);
 
+  // USE EFFECTS
   useEffect(() => {
+    const currentHash = window.location.hash;
     const targetElement = document.getElementById(currentHash.substring(1));
 
     if (targetElement) {
@@ -17,16 +26,36 @@ const HomePage = () => {
         block: "start",
       });
     }
-  }, [currentHash]);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPopupOpen(true);
+    }, 500);
+  }, []);
+
+  // FUNCTIONS
+  const togglePopup = () => {
+    setPopupOpen(!popupOpen);
+  };
 
   return (
-    <>
-      <Home />
+    <div className={homepageContainer}>
+      {popupOpen && (
+        <Popup
+          eventPosterURL="/img/events/live/valo.png"
+          eventHeading="Valorant"
+          eventText="ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed"
+          eventFormURL="/events/valorant"
+          closePopup={togglePopup}
+        />
+      )}
+      <Home showTrigger={!popupOpen} openPopup={togglePopup} />
       <About />
       <HomeEvents />
       <Alliance />
       <HomeBlogs />
-    </>
+    </div>
   );
 };
 
