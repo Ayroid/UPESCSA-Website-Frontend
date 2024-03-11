64a519da887245c7b5329f65537e40de27862671
priii-25 <121582683+priii-25@users.noreply.github.com>
2024-03-12 01:16:18 +0530

    Evortal Updated with Funtopia

--- src/pages/EvortalPage/EvortalPage.jsx
+++ src/pages/EvortalPage/EvortalPage.jsx
@@ -28,39 +28,39 @@ const EvortalPage = () => {
     //   activeEvent: true,
     // },
     {
-      eventImageURL: "/img/events/live/Hackersummit.jpg",
+      eventImageURL: "/img/events/live/FRENZY_PITCH-01.jpg",
       eventHeading: "Frenzy Pitch",
       eventText:
-        "UPES Cloud Security Alliance, in collaboration with DEF CON Delhi Group, with great enthusiasm extends an invitation to join on a significant venture – Hackers Summit 1.0. We aim to extend valuable prospects to young minds, aiding them in learning within the paramount realm of the Cyber World - encompassing “the Holy Grail” of the Cyber World i.e.  Protect, detect, and respond.A one-day with the core objective of providing a comprehensive educational platform for students, Hackers Summit is an initiative to bring these minds under one umbrella through engaging activities and proffer networking opportunities. From foundational principles to intermediate-level concepts, we've got you covered!  This one day venture will span from foundational principles to intermediate-level concepts, ensuring a comprehensive learning experience. Hands-on demonstrations will allow participants to apply their knowledge in practical scenarios, enhancing their skills in the dynamic field of cybersecurity.So, are you ready to embark on an exhilarating journey into the realm of cybersecurity? Mark your calendars and make a date with knowledge and innovation at Hackers Summit!",
+        "A competition that challenges teams to rethink and redesign ordinary household items into innovative products, with a wildcard challenge to push your creative boundaries.",
       eventMode: "Offline",
       eventTeamSize: "2-4",
       eventRegistrationFee: "Paid",
       eventRegistrationType: "Mixed",
-      eventDate: "16 Mar",
+      eventDate: "19-20 Mar",
       eventFormURL: "/evortal/frenzypitch",
       activeEvent: true,
     },
     {
-      eventImageURL: "/img/events/live/Hackersummit.jpg",
+      eventImageURL: "/img/events/live/Virtual Escape FUNTOPIA.jpg",
       eventHeading: "Virtual Escape Room",
-      eventText: "?",
+      eventText: "A competition that challenges teams to rethink and redesign ordinary household items into innovative products, with a wildcard challenge to push your creative boundaries.",
       eventMode: "Offline",
       eventTeamSize: "1-2",
       eventRegistrationFee: "Paid",
       eventRegistrationType: "Mixed",
-      eventDate: "16 Mar",
+      eventDate: "19-20 Mar",
       eventFormURL: "/evortal/virtualescaperoom",
       activeEvent: true,
     },
     {
-      eventImageURL: "/img/events/live/Hackersummit.jpg",
+      eventImageURL: "/img/events/live/Ultimate_Showdown-01.jpg",
       eventHeading: "The Ultimate Showdown",
-      eventText: "?",
+      eventText: 'Engage in a physical and mental test through the "Floor is Lava" and "Stack It Challenge", designed to test your agility, precision, and strategy.',
       eventMode: "Offline",
       eventTeamSize: "1",
       eventRegistrationFee: "Paid",
       eventRegistrationType: "Mixed",
-      eventDate: "16 Mar",
+      eventDate: "19-20 Mar",
       eventFormURL: "/evortal/theultimateshowdown",
       activeEvent: true,
     },
@@ -81,7 +81,7 @@ const EvortalPage = () => {
 
   return (
     <div className={evortalContainer}>
-      <PageHeading imgURL="/img/pageheaders/evortal.jpg" text="EVORTAL" />
+      <PageHeading imgURL="/img/events/live/FUNTOPIA 4.0 BANNER-01.jpg" text="EVORTAL" />
       <div className={evortalDiv}>
         <EvortalSeparator text="Active Events" />
         {activeEvents.map((event) => (
