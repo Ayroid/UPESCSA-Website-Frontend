3ae88245fd0a59645014f4ce118cc0b32ae89d37
priii-25 <121582683+priii-25@users.noreply.github.com>
2024-04-12 16:39:17 +0530

    Problem Statements

---
+++ src/components/ProblemStatementSelection/ProblemStatementSelection.jsx
@@ -0,0 +1,50 @@
+import Proptypes from "prop-types";
+import { Link } from "react-router-dom";
+import styles from "./ProblemStatementSelection.module.css";
+
+// CSS STYLES
+const {
+  popupContainer,
+  popupDiv,
+  popupTextDiv,
+  popupHeading,
+  popupText,
+  popupRegisterButton,
+  popupCloseButton,
+} = styles;
+
+const Popup = ({
+  eventPosterURL,
+  eventHeading,
+  eventText,
+  eventFormURL,
+  closePopup,
+}) => {
+  return (
+    <div className={popupContainer}>
+      <div className={popupDiv}>
+        <div className={popupTextDiv}>
+          <h4 className={popupHeading}>{eventHeading}</h4>
+          <p className={popupText}>{eventText}</p>
+          <Link to={eventFormURL} className={popupRegisterButton}>
+            <p>Choose Problem Statement</p>
+            <img loading="lazy" src="/icons/rightArrow.png" alt="arrow" />
+          </Link>
+        </div>
+        <div className={popupCloseButton} onClick={closePopup}>
+          <img loading="lazy" src="/icons/hamburgercross.png" alt="cross" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Popup.propTypes = {
+  eventPosterURL: Proptypes.string.isRequired,
+  eventHeading: Proptypes.string.isRequired,
+  eventText: Proptypes.element.isRequired,
+  eventFormURL: Proptypes.string.isRequired,
+  closePopup: Proptypes.func.isRequired,
+};
+
+export default Popup;
--- src/sections/ProblemStatement/ProblemStatement.jsx
+++ src/sections/ProblemStatement/ProblemStatement.jsx
@@ -1,5 +1,5 @@
 import styles from "./ProblemStatement.module.css";
-import React, { useState, useRef } from "react";
+import React from "react";
 
 const {
   mainDiv,
@@ -7,13 +7,9 @@ const {
   problemStatementInfo,
   problemStatementTitles,
   problemStatementHeading,
-  progressIndicator,
 } = styles;
 
 const ProblemStatement = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const panelRef = useRef(null);
-
   const panel = [
     {
       id: "PS001",
@@ -42,96 +38,21 @@ const ProblemStatement = () => {
       functionalRequirement: "Functional requirement of the solution.",
       nonFunctionalRequirement: "Non-functional requirement of the solution.",
     },
-    {
-      id: "PS001",
-      title: "Problem Statement Title",
-      byOrganization: "Organization Name",
-      sectorDomain: "Sector/Domain",
-      problemDescription: "This is the problem description.",
-      functionalRequirement: "Functional requirement of the solution.",
-      nonFunctionalRequirement: "Non-functional requirement of the solution.",
-    },
-    {
-      id: "PS001",
-      title: "Problem Statement Title",
-      byOrganization: "Organization Name",
-      sectorDomain: "Sector/Domain",
-      problemDescription: "This is the problem description.",
-      functionalRequirement: "Functional requirement of the solution.",
-      nonFunctionalRequirement: "Non-functional requirement of the solution.",
-    },
-    {
-      id: "PS001",
-      title: "Problem Statement Title",
-      byOrganization: "Organization Name",
-      sectorDomain: "Sector/Domain",
-      problemDescription: "This is the problem description.",
-      functionalRequirement: "Functional requirement of the solution.",
-      nonFunctionalRequirement: "Non-functional requirement of the solution.",
-    },
-    {
-      id: "PS001",
-      title: "Problem Statement Title",
-      byOrganization: "Organization Name",
-      sectorDomain: "Sector/Domain",
-      problemDescription: "This is the problem description.",
-      functionalRequirement: "Functional requirement of the solution.",
-      nonFunctionalRequirement: "Non-functional requirement of the solution.",
-    },
-    {
-      id: "PS001",
-      title: "Problem Statement Title",
-      byOrganization: "Organization Name",
-      sectorDomain: "Sector/Domain",
-      problemDescription: "This is the problem description.",
-      functionalRequirement: "Functional requirement of the solution.",
-      nonFunctionalRequirement: "Non-functional requirement of the solution.",
-    },
-    {
-      id: "PS001",
-      title: "Problem Statement Title",
-      byOrganization: "Organization Name",
-      sectorDomain: "Sector/Domain",
-      problemDescription: "This is the problem description.",
-      functionalRequirement: "Functional requirement of the solution.",
-      nonFunctionalRequirement: "Non-functional requirement of the solution.",
-    },
-  ];
-  const handleScroll = (e) => {
-    const container = e.target;
-    const slideWidth = container.offsetWidth;
-    const scrollLeft = container.scrollLeft;
-    const currentSlide = Math.round(scrollLeft / slideWidth);
-    setCurrentSlide(currentSlide);
-  };
-  const handleMouseWheel = (e) => {
-    e.preventDefault();
-    const container = panelRef.current;
-    const delta = e.deltaY;
-    const scrollLeft = container.scrollLeft;
-    container.scrollLeft = scrollLeft + delta;
-  };
+    ];
+
   return (
     <div className={mainDiv} id="problemStatement">
       <div className={problemStatementHeading}>
-        <h1 className="heading" id={problemStatementHeading}>
+        <h1 className="heading">
           Problem Statements
         </h1>
       </div>
-      <div className={progressIndicator}>
-        {currentSlide + 1} / {panel.length}
-      </div>
-      <div
-        className={problemStatementDiv}
-        onScroll={handleScroll}
-        onWheel={handleMouseWheel}
-        ref={panelRef}
-      >
+      <div className={problemStatementDiv}>
         {panel.map((statement, index) => (
           <div className={problemStatementInfo} key={index}>
             <h3>PS ID: {statement.id}</h3>
             <div className={problemStatementTitles}>
-              <h3>PS Title:{statement.title}</h3>
+              <h3>PS Title: {statement.title}</h3>
             </div>
             <p>PS By Organization: {statement.byOrganization}</p>
             <p>PS Sector/Domain: {statement.sectorDomain}</p>
